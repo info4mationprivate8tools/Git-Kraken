ac32c9e73cbee6818d89bff7461aa0ba542289ae
info4mationprivate8tools <xcybernett@gmail.com>
2022-12-28 19:33:55 +0530

    Connected With Github

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int a;
-        a = 5;
-        System.out.println(a);
+        int nulled;
+        nulled = 5;
+        System.out.println(nulled);
     }
 }
